2a9b7e2fbd883c48384adace1df8833ba6ba6137
Gabriel Estevão Nunes <gabrielestevo92@gmail.com>
2024-05-05 16:09:40 -0300

    feat: config sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,31 @@
 
 import { Header } from './components/header/header'
-
+import { Experience } from './components/experience/experience';
 import "./styles/home.scss"
 
 export default function Home() {
   return (
     <main className="container">
       <Header/>
-      <div className="experience">
-        <h3>Experience</h3>
-        <p>texto</p>
-        <div className="experience-time">
+      <Experience/>
+      <div className="infos">
+        <h3>Languages</h3>
+        <div className="languages-info">
+          <span>🇺🇸 EN - Basic</span>
+          <span>🇧🇷 PT-BR - Native Speaker</span>
+        </div>
+        <h3>Education</h3>
+        <div className="educational-info">
+          <span>🎓️</span>
+          <span>Sistemas para internet - Centro Universitario de João Pessoa - Unipê</span>
+        </div>
+        <span></span>
+      </div>
+      <div className="buttons">
+        <div className="social">
 
         </div>
-        <div className="infos">
-          <h3>Languages</h3>
-          <div className="languages-info">
-            <span>🇺🇸 EN - Basic</span>
-            <span>🇧🇷 PT-BR - Native Speaker</span>
-          </div>
-          <h3>Education</h3>
-          <div className="educational-info">
-            <span>🎓️</span>
-            <span>Sistemas para internet - Centro Universitario de João Pessoa - Unipê</span>
-          </div>
-          <span></span>
-        </div>
-        <div className="buttons">
-          <div className="social">
-
-          </div>
-          <button>Contact me</button>
-        </div>
+        <button>Contact me</button>
       </div>
       
     </main>
