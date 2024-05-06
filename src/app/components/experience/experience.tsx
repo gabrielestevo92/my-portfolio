66b5c99866baa23a73c4d7354eafa3bb08d9be68
Gabriel Estevão Nunes <gabrielestevo92@gmail.com>
2024-05-05 22:48:30 -0300

    feat: experience measure

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { SectionTitle } from "../sectionTitle/section-title";
 import "./experience.scss"
 
@@ -7,7 +8,90 @@ export function Experience(){
             <SectionTitle text="Experience"/>
             <p>1 year working as web developer freelancer</p>
             <div className="experience-time">
-
+                <div className="experience-language">
+                    <Image
+                    src="/python.png"
+                    alt= "Python"
+                    width={40}
+                    height={40}
+                    priority
+                    />
+                    <div className="experience-unit">
+                        <div className="experience-measure measure-1">
+                            <span>1 year</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="experience-language">
+                    <Image
+                    src="/django.png"
+                    alt= "Django"
+                    width={40}
+                    height={40}
+                    priority
+                    />
+                    <div className="experience-unit">
+                        <div className="experience-measure measure-1">
+                            <span>1 year</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="experience-language">
+                    <Image
+                        src="/js.png"
+                        alt= "JavaScript"
+                        width={40}
+                        height={40}
+                        priority
+                        />
+                    <div className="experience-unit">
+                        <div className="experience-measure measure-1">
+                            <span>1 year</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="experience-language">
+                    <Image
+                        src="/ts.png"
+                        alt= "TypeScript"
+                        width={40}
+                        height={40}
+                        priority
+                        />
+                    <div className="experience-unit">
+                        <div className="experience-measure measure-m">
+                            <span>Starting</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="experience-language">
+                    <Image
+                        src="/react.png"
+                        alt= "React"
+                        width={40}
+                        height={40}
+                        priority
+                        />
+                    <div className="experience-unit">
+                        <div className="experience-measure">
+                            <span>Starting</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="experience-language">
+                    <Image
+                        src="/next.png"
+                        alt= "Next"
+                        width={40}
+                        height={40}
+                        priority
+                        />
+                    <div className="experience-unit">
+                        <div className="experience-measure">
+                            <span>Starting</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/app/components/social-btns/social-btns.tsx
+++ src/app/components/social-btns/social-btns.tsx
@@ -9,8 +9,8 @@ export function SocialBtns(){
     return(
         <div className="social">
             <a href="https://www.instagram.com/gabrielestevo94/"><InstaIcon/></a>
-            <a href=""><LinkedinIcon/></a>
-            <a href=""><GitIcon/></a>
+            <a href="https://www.linkedin.com/in/gabriel-estev%C3%A3o-nunes-764570207/"><LinkedinIcon/></a>
+            <a href="https://github.com/gabrielestevo92"><GitIcon/></a>
             <a href="https://wa.me/5583999887282"><WhatsappIcon/></a>
         </div>
     )
